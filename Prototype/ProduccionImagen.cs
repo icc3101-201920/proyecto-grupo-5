2106f727701b82d87b4acf480fdb73c487c2c715
Jose Miguel Iturralde <jmiturralde@miuandes.cl>
2019-09-24 11:14:09 -0300

    Yiyo 1

---
+++ Prototype/ProduccionImagen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    class ProduccionImagen
+    {
+        private List<Imagen> imagenes;
+        private string color;
+
+        public ProduccionImagen(List<Imagen> imagenes, string color)
+        {
+            this.imagenes = imagenes;
+            this.color = color;
+        }
+
+        public List<Imagen> Imagenes { get => imagenes; set => imagenes= value; }
+        public string Color { get => color; set => color = value; }
+
+    }
+}
