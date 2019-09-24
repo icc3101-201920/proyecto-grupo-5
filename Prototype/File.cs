905085df250e156f5763437565fc3a7c883a52cb
rcorrea4 <rcorrea4@miuandes.cl>
2019-09-24 11:23:29 -0300

    file normaltag

---
+++ Prototype/File.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    class File
+    {
+        private string nombre;
+        private string direccionmemoria;
+
+        public File(string nombre,string direccionmemoria)
+        {
+            this.direccionmemoria = direccionmemoria;
+            this.nombre = nombre;
+
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccionmemoria { get => direccionmemoria; set => direccionmemoria = value; }
+
+
+    }
+}
